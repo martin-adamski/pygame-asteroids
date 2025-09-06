1a81d95bbea385214d33d80f30cb494140920c4a
martin-adamski <martin.p.adamski@gmail.com>
2025-09-06 21:35:47 +0100

    stationary triangle, rotates clockwise and anticlockwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player
 
 def main():
     print("Starting Asteroids!")
@@ -7,6 +8,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    player_obj = player.Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -17,7 +21,14 @@ def main():
                 return
 
         screen.fill('black')
+
+        player_obj.draw(screen)
+
+        player_obj.update(dt)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
